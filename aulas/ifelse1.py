3999eed6344e12545c84e3fa3491342a21d28dbb
boo <studiiesboo@gmail.com>
2025-09-03 20:08:55 -0300

    adding archieves

---
+++ aulas/ifelse1.py
@@ -0,0 +1,4 @@
+a= int(input('digite um valor numerico: '))
+if (a < 10): #se digitar um num menor que 10 vai abrir a função do if
+    print('vc digitou um numero menor que 10') #só vai triggar se vc inserir um numero menor que 10, senao vai direto pra linha 4
+print('fim do programa')
---
+++ aulas/ifelse2.py
@@ -0,0 +1,7 @@
+a = int(input('digite um valor numerico: '))
+if (a < 10):
+    print('vc digitou um numero menor que 10')
+    #se for digitado um numero menor que 10 trigga o bloco de função IF
+else: #se for um numero fora do escopo do if (que não seja menor que 10)
+    print('vc digitou um numero maior ou igual a 10')
+print('fim do programa')
