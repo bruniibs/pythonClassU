45c0137e358f22ca87507928b6adb4db29f5ff63
boo <studiiesboo@gmail.com>
2025-09-03 21:21:27 -0300

    add

---
+++ aulas/ifelseEx2.py
@@ -0,0 +1,8 @@
+nota = float(input('insira sua nota: '))
+
+if (nota >= 60):
+    print('vc foi aprovado')
+elif(nota >= 40):
+    print('vc ficou de exame')
+else:
+    print('vc foi reprovado')
---
+++ aulas/ifelseex1.py
@@ -0,0 +1,9 @@
+num = int(input('insira um numero: '))
+
+if (num > 0):
+    print('seu numero é positivo')
+elif(num == 0):
+    print('seu numero é Zero')
+else:
+    print('seu numero é negativo')
+print('fim do programa')
